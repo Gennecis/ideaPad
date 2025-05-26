fdd814280a4d3eaf71db72c8ed737a7c978e92dc
Gennecis <okeziepascal2@gmail.com>
2025-05-26 14:57:28 +0100

    iclude tab title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ideaPad - Capture Brilliance',
+  description: 'Turn sparks of genius into organized ideas. Effortlessly.',
+  generator: 'ideaPad',
 }
 
 export default function RootLayout({
